78228c4c578e8c47f944f12ebf496b25a07be9af
Marco Lopes <124166149+MarcoLSantos@users.noreply.github.com>
2025-07-08 18:31:47 -0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+// Exemplo de rota simples
+$app->get('/teste', function ($request, $response) {
+    $response->getBody()->write("🚀 API Slim funcionando!");
+    return $response;
+});
+
+// Incluir rotas externas
+require __DIR__ . '/routes/ramal.php';
+
+$app->run();
