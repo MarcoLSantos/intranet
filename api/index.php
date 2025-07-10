fc4d8b3c0b7baaab15c28cb275100c7c47d60d72
Marco Lopes <124166149+MarcoLSantos@users.noreply.github.com>
2025-07-10 16:05:29 -0300

    Add files via upload

--- api/index.php
+++ api/index.php
@@ -1,15 +1,16 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 $app->setBasePath('/api/index.php');
+
+// ✅ Middleware necessário
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-// Inclui as rotas passando $app
+// 🔗 Rotas externas
 (require __DIR__ . '/routes/ramal.php')($app);
 (require __DIR__ . '/routes/sugestao.php')($app);
 (require __DIR__ . '/routes/moderacao.php')($app);
