1c4ddea640aef37a221ca5ab94d805978c096772
Marco Lopes <124166149+MarcoLSantos@users.noreply.github.com>
2025-07-25 14:48:43 -0300

    Add files via upload

---
+++ inc/iframe_scih.php
@@ -0,0 +1,10 @@
+<div class="card p-4" style="max-width:1000px; margin:auto;">
+  <h4 style="font-size:18px;">📊 Precauções - SCIH</h4>
+  <iframe 
+    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRZb4k6bRAKhvTS5a6VTVf_eh_DQNrsTl-U60jHsCm219AnfkoaEhBsbhs9BmUnlABNX5UWDONTTu0c/pubhtml?gid=1276835806&single=true"
+    width="100%" 
+    height="650" 
+    frameborder="0" 
+    style="border:1px solid #ccc; border-radius:6px;">
+  </iframe>
+</div>
